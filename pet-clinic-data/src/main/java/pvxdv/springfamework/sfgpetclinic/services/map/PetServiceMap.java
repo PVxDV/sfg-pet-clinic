dcaae21955cb9f86ef86b4ff1402983e57a1a816
Pavel Rizhikh <rizhikh.pavel@yandex.ru>
2022-05-21 18:34:23 +0300

    Implement Map based Services. Closes #6.

---
+++ pet-clinic-data/src/main/java/pvxdv/springfamework/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package pvxdv.springfamework.sfgpetclinic.services.map;
+
+import pvxdv.springfamework.sfgpetclinic.model.Pet;
+import pvxdv.springfamework.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findByID(id);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet.getId(), pet);
+    }
+
+    @Override
+    public void delete(Pet pet) {
+        super.delete(pet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
