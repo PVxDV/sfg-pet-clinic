479da19377b4c53b88c828fee681aff8cbf9a7e8
Pavel Rizhikh <rizhikh.pavel@yandex.ru>
2022-06-24 00:23:06 +0300

    Added PetTypeService. Closes #29.

---
+++ pet-clinic-data/src/main/java/pvxdv/springfamework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package pvxdv.springfamework.sfgpetclinic.services;
+
+import pvxdv.springfamework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
